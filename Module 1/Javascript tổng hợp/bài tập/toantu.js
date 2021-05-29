84b877aeace8e0ce7b3fdeddea2203011feee553
An Nguyen <annguyen@An-s.local>
2021-05-29 16:39:43 +0700

    nop bai tap

--- Module 1/Javascript tổng hợp/bài tập/toantu.js
+++
@@ -1,8 +0,0 @@
-let inputWidth;
-let inputHeight;
-inputWidth=prompt('nhập Width:');
-inputHeight=prompt('nhập Height:');
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
-let area = width * height;
-document.write("The area is: " + area);
